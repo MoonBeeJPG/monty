f843f04ed01ebcc61a523ee1691511c5e434e3b9
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-12 14:19:26 -0700

    mandatory opcodes

--- monty.h
+++ monty.h
@@ -7,12 +7,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
-
 #include <stdbool.h>
 #include <string.h>
-
 #define DELIM "\n \r\t"
-
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -52,6 +49,10 @@ void array(stack_t **stack, char *line, unsigned int number_line);
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 #endif
