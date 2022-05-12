7bc7bbb0604317660e34f3027639a4a2cdde0af2
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-12 09:17:10 -0700

    main checked

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+#include "monty.h"
+/**
+* main - Main function
+*
+* @argc: Quantity of arguments passed to the program
+* @argv: Arguments vector
+*
+* Return: On success, EXIT_SUCCESS, on failure, EXIT_FAILURE
+*/
+int main(int argc, char *argv[])
+{
+	stack_t *header;
 
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	header = NULL;
+
+	readit(argv[1], &header);
+	exit(EXIT_SUCCESS);
+}
