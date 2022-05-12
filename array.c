08178bb04516ecbd0ea4e492c3fa402d0bcccd2e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-12 08:53:04 -0700

    pall and push implemented in the array

--- array.c
+++ array.c
@@ -10,5 +10,25 @@
 */
 void array(stack_t **stack, char *line, unsigned int number_line)
 {
+	int i;
 
+	instruction_t array_f[] = {
+		{"push", push},
+		{"pall" pall},
+		{NULL, NULL}
+	};
+
+	for (i = 0; array_t[i].opcode; i++)
+	{
+		if (strcmp(line, array_f[i].opcode) == 0)
+		{
+			array_f[i].f(stack, number_line);
+			return;
+		}
+	}
+	if (strlen(line) != 0 && line[0] != '#')
+	{
+		printf("L%u: unknown instruction %s\n", numberline, line);
+		exit (EXIT_FAILURE);
+	}
 }
