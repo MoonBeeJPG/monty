aabc06d71483de64d8f251c705b17cc13565ae66
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-12 14:46:10 -0700

    a

--- array.c
+++ array.c
@@ -33,6 +33,7 @@ void array(stack_t **stack, char *line, unsigned int number_line)
 	if (strlen(line) != 0 && line[0] != '#')
 	{
 		printf("L%u: unknown instruction %s\n", number_line, line);
+		free(stack);
 		exit(EXIT_FAILURE);
 	}
 }
