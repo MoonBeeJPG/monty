cdc7e7a6e48a7d6d92c51cee95b714bc7bb782b6
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-12 13:10:17 -0700

    fixing problems

--- push-pall_func.c
+++ push-pall_func.c
@@ -41,10 +41,12 @@ void push(stack_t **stack, unsigned int line_number)
 * @stack: Doubly linked list representation of the stack (or queue)
 * @line_number: Number of lines passed
 */
-void pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
 	
+	(void) line_number;
+
 	while (current)
 	{
 		printf("%d\n", current->n);
